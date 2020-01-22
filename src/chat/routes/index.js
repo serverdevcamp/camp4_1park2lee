8af244572085361ef30e9d7240b827f6484feff5
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-01-22 19:11:58 +0900

    HM1-make chat_with_user_name_in_room

--- src/chat/routes/index.js
+++ src/chat/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var room = require('../model/room');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('temp.html');
