be11a34a2f6820858c1e0eb182d1ae58fea7e46a
gomamon <leeye9779@gmail.com>
2020-01-23 15:02:21 +0900

    project setup

---
+++ src/auth/config.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+    db : {
+        host : 'localhost',
+        user : 'admin',
+        password : 'aa970709',
+        port : '',
+        database: 'db_auth'
+    },
+    redis : {
+        host : '127.0.0.1',
+        port : 6379,
+        db : 0,
+        password :'aa970709'
+    },
+    jwt : {
+        certification: 'yeddojjang',
+        reset: 'jjangjjang',
+    },
+    kakao : {
+        key : 'd18d147fa7364f14a4461bde45486b1a'
+    },
+    gmail :{
+        email: 'leeye9779@gmail.com',
+        password: 'tooth970709'
+    }
+};
