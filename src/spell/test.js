e582d47043dc249f2f718a87575ec5b95ac03f46
Below0 <l4538@naver.com>
2020-01-24 02:02:12 +0900

    Modify spell module

--- src/spell/test.js
+++ src/spell/test.js
@@ -1,9 +1,15 @@
 const hanspell = require('hanspell');
 
-const sentence = '안녕하 세요';
+const sentence = '안녕하세요';
 const end = function () { console.log("<ends>"); };
 const error = function (err) { console.error("<error: " + err + ">"); };
 
+async function spellCheck(context){
+    let result = await hanspell.spellCheckByPNU(context, 6000, console.log, end, error);
+    console.log('funct'+result);
+    return result;
+}
 
-result = hanspell.spellCheckByPNU(sentence, 6000, console.log, end, error);
-console.log(result)
+
+ result = spellCheck(sentence);
+ console.log(res);
