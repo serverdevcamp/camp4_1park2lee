d9649030cffe3b3a7a9492e9d289dbae45068bd0
Below0 <l4538@naver.com>
2020-01-24 02:02:12 +0900

    Modify spell module

--- src/spell/routes/spell.js
+++ src/spell/routes/spell.js
@@ -12,9 +12,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/',function(req, res, next){
- 
- 
-  res.send(req.body)
+  var result; // 교정 결과 Object
+  var context = req.body['context'];
+  var errCount = 0; // 에러 카운트
+  
+  hanspell.spellCheckByPNU(context, 6000, console.log, end, error, function(message){
+    result = message;
+});
+
+  for(var i = 0; i < result.length; i++){
+    errCount += result[i].length;
+    for(var j = 0; j < result[i].length; j++){
+      context = context.replace(result[i][j]['token'], result[i][j]['suggestions'][0]);
+    }
+  }
+
+  res.send(context);
 })
 
 module.exports = router;
