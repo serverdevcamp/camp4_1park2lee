33a2f8efdff90299cdc5b89501b96a626f771474
gomamon <leeye9779@gmail.com>
2020-01-28 02:35:10 +0900

    vuejs routing with auth server

--- src/auth/routes/users.js
+++ src/auth/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../data/user.json');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(users);
 });
+
+
+router.get('/:id', function (req, res, next){
+	var id = parseInt(req.params.id, 10)
+	var user = users.filter(function (user) {
+		return user.id === id
+	});
+	res.send(user);
+});
+
 
 module.exports = router;
