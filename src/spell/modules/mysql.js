16babd46998d3091dea5def75a53ddc67191b6c2
Below0 <l4538@naver.com>
2020-01-28 10:18:17 +0900

    change to use db pool

--- src/spell/modules/mysql.js
+++ src/spell/modules/mysql.js
@@ -1,19 +1,15 @@
 var mysql = require('mysql');
 const db_config = require('../db-config.json')
-var connection = mysql.createConnection({
-  host     : db_config.host,
-  user     : db_config.user,
-  password : db_config.password,
-  port     : db_config.port,
-  database : db_config.database
-});
-connection.connect();
+var pool = mysql.createPool(db_config);
 
  function getWords(data){
-    connection.query("SELECT DISTINCT * FROM Words WHERE original = ? AND checked = ?",data, function(err, rows, fields) {
+  pool.getConnection(function(err,connection){
+    if(!err){
+      connection.query("SELECT DISTINCT * FROM Words WHERE original = ? AND checked = ?",data, function(err, rows, fields) {
         if (!err){
+          var row_id;
           if(rows.length > 0){
-            var row_id = rows[0]['id'];
+             row_id = rows[0]['id'];
             connection.query("UPDATE Words SET error = error + 1 WHERE id = ?",row_id, function(err, rows, fields) {
               if (!err)
                 console.log('errcount: ',rows);
@@ -22,9 +18,10 @@ connection.connect();
             });
           }
           else{
-            connection.query("INSERT INTO Words(original, checked) Values (?)",[data], function(err, rows, fields) {
-              if (!err)
-                console.log('New word INSERT');
+            connection.query("INSERT INTO Words(original, checked) Values (?)",[data], function(err, row, fields) {
+              if (!err){
+                console.log('New word INSERT: ',row['insertId']);
+              }
               else
                 console.log('Error while performing Query[INSERT].', err);
             });
@@ -33,6 +30,10 @@ connection.connect();
         else
           console.log('Error while performing Query[SELECT].', err);
       });
+    }
+    // 커넥션을 풀에 반환
+    connection.release();
+  });
 }
 
 module.exports = {
