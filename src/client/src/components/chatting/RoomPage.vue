7040f85ac685ae726de00649fb969613a9fce5cb
gomamon <leeye9779@gmail.com>
2020-02-02 17:42:43 +0900

    get chatting by api

--- src/client/src/components/chatting/RoomPage.vue
+++ src/client/src/components/chatting/RoomPage.vue
@@ -1,17 +1,16 @@
 <template>
 
 <div>
-  <h2>{{username}}</h2>
+  <h2>{{user_name}}</h2>
   <div class="card">'
-  {{data}}
+  <p>test: {{test}}</p>
+  <p>message : {{messages}}</p>
   <!-- <p>socket:{{socket}}||{{data}}{{messages}}</p> -->
-  {{data}}
-  {{messages}}
   <p></p>
   <li class="list-group-item" v-for="message in messages" :key="message">
-      <span :class="{'float-left':message.type === 1}">
-          {{message.message}}
-          <small>:{{message.user}}</small>
+      <span >
+        {{message.chatMsg}}
+        <small>:{{message.chatUserName}}</small>
       </span>
   </li>
   </div>
@@ -31,16 +30,16 @@ import io from 'socket.io-client';
 
 export default {
   name: 'Room',
-  // created: function(){
-  //   let user_id = this.$route.params.user_id;
-  //   let room_number = this.$route.params.room_number;
-  //   this.$http.get(`/api/room/${user_id}/${room_number}`)
-  //   .then((response)=>{
-  //       this.user_id = response.data.userId;
-  //       this.user_name = response.data.userName;
-  //   })
-    
-  // },
+  created: function(){
+    let user_id = this.$route.params.user_id;
+    let room_number = this.$route.params.room_number;
+    this.$http.get(`/api/room/${user_id}/${room_number}`)
+    .then((response)=>{
+        this.user_id = user_id;
+        this.user_name = user_id;
+        this.messages = response.data.chatList;
+    })
+  },
 
   data: function(){
     let user_id = this.$route.params.user_id;
@@ -49,7 +48,8 @@ export default {
       user_id: '',
       user_name: '',
       messages: [],
-      data:'',
+      test: '',
+      api_messages:[],
       // socket: io('http://127.0.0.1:3000')
       socket: io(`http://127.0.0.1:3000?room=${room_number}&user=${user_id}`)
       //socket: io(`http://127.0.0.1:3000?user=${user_id}`)
@@ -58,9 +58,9 @@ export default {
   methods: {
     send() {
       this.messages.push({
-          message: this.newMessage,
-          type: 0,
-          user: 'Me',
+          chatUserName: this.user_name,
+          chatUserId: this.user_id,
+          chatMsg: this.newMessage,
       });
       this.socket.emit('chat message', {
           msg: this.newMessage     
@@ -69,26 +69,33 @@ export default {
   },
   mounted(){
     this.socket.emit('chat enter');
-    // this.socket.on('server chat enter', function (data) {
-      // this.data = data;
-      // this.messages.push({
-      //     message: `${data.user}님이 입장하셨습니다.`,
-      //     type: 0,
-      //     user: data.user
-      //   });
-    // });
-    this.socket.on('server chat message', function (data) {
-        this.messages.push({
-          message: data.msg,
-          type: 1,
-          user: data.user
+
+    this.socket.on('test', function (data) {
+      this.test = data        
+    });
+
+    this.socket.on('server chat enter', function (data) {
+      this.data = data;
+      this.messages.push({
+          chatMsg: `${data.user}님이 입장하셨습니다.`,
+          chatUserName: data.user,
+          chatUserId: data.user
         });
     });
+
+    this.socket.on('server chat message', function (data) {
+        this.messages.push({
+          chatMsg: `${data.user}님이 입장하셨습니다.`,
+          chatUserName: data.user,
+          chatUserId: data.user
+        });
+    });
+
+    
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
