b3bff98e38e8b87e559e271d3aff020d58a0032d
Below0 <l4538@naver.com>
2020-02-19 13:47:10 +0900

    move files

--- src/spell/utils/spellUtils.js
+++ src/spell/utils/spellUtils.js
@@ -3,7 +3,7 @@ const pattern = /^[a-zA-Zㄱ-힣0-9\s]*$/;
 
 module.exports = {
     filter: (msg) => {
-        return msg.replace(/(^[\sa-zA-Z!?:;'"#$%^&*~`.,<>\^]*)|([\sa-zA-Z!?;'"#$%^&*~`.,<>\^]*$)/gi, "");
+        return msg.replace(/(^[\sㄱ-ㅎa-zA-Z!?:;'"#$%^&*~`.,<>\^]*)|([\sㄱ-ㅎa-zA-Z!?;'"#$%^&*~`.,<>\^]*$)/gi, "");
     },
     saveFilter: (msg) => {
         return pattern.test(msg);
