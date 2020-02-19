2d26014cd3e5811e2c2e2c329f0ece07a84098b8
gomamon <leeye9779@gmail.com>
2020-02-19 13:00:42 +0900

    update config

--- src/hunmin-config.js
+++ src/hunmin-config.js
@@ -1,14 +1,15 @@
 let path = require('path');
+let NODE_ENV = "development";
 
 module.exports = {
-    NODE_ENV : "development",
-    IP : (this.NODE_ENV === "development") ? "localhost": "localhost",
-    AUTH_PORT : (this.NODE_ENV === "development") ? 3300 : 3300,
-    CHAT_PORT : (this.NODE_ENV === "development") ? 3000 : 3000,
-    SPELL_PORT : (this.NODE_ENV === "development") ? 3100 : 3100,
-    AUTH_URL : (this.NODE_ENV === "development") ? "localhost:3300" : "localhost:3300",
-    CHAT_URL : (this.NODE_ENV === "development") ? "localhost:3000" : "localhost:3000",
-    SPELL_URL : (this.NODE_ENV === "development") ? "localhost:3100" : "localhost:3100",
-    CONFIG_PATH : path.join(__dirname , '../config/'),
-    IMAGES_PATH : path.join(__dirname, '../data/images')
+    NODE_ENV: NODE_ENV,
+    IP: (NODE_ENV === "development") ? "localhost" : "34.219.138.15",
+    AUTH_PORT: (NODE_ENV === "development") ? 3300 : 3300,
+    CHAT_PORT: (NODE_ENV === "development") ? 3000 : 3000,
+    SPELL_PORT: (NODE_ENV === "development") ? 3100 : 3100,
+    AUTH_URL: (NODE_ENV === "development") ? "localhost:3300" : "34.219.138.15:3300",
+    CHAT_URL: (NODE_ENV === "development") ? "localhost:3000" : "34.219.138.15:3000",
+    SPELL_URL: (NODE_ENV === "development") ? "localhost:3100" : "34.219.138.15:3100",
+    CONFIG_PATH: path.join(__dirname, '../../../config/'),
+    IMAGES_PATH: path.join(__dirname, '../../../data/images')
 };
